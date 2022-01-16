ab323b07ec7e5f2401232b45ff5d8e5713e6437e
Anoop072001 <anoopsidhan777@gmail.com>
2022-01-16 17:53:10 +0530

    new

--- calc.py
+++ calc.py
@@ -210,3 +210,11 @@
 #     print("Sunday")
 # else:
 #     print("wrong number")
+
+# n = int(input())
+# if(n == 1 or n == 3 or n == 5 or n == 7 or n == 8 or n == 10 or n == 12):
+#     print("31 days")
+# elif(n == 2):
+#     print("28 days")
+# else:
+#     print("30 days")
